f3c4a621eb6413e486f5fa25629e10fab65b20a9
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-24 23:42:06 +0300

    Product.js fixed.

--- core/src/components/Products.js
+++ core/src/components/Products.js
@@ -4,6 +4,7 @@ import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'react
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [productGroups, setProductGroups] = useState([]);
 
   useEffect(() => {
     api.get('/api/products/')
@@ -15,46 +16,58 @@ function Products() {
       });
   }, []);
 
+  useEffect(() => {
+    // Group products into sets of 4
+    const groupedProducts = [];
+    for (let i = 0; i < products.length; i += 4) {
+      groupedProducts.push(products.slice(i, i + 4));
+    }
+    setProductGroups(groupedProducts);
+  }, [products]);
+
   return (
-    <div>
-      <h1>Products</h1>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <div>
+        <h1>Products</h1>
 
-
-      {products.map(product => (
-        <Card
-          style={{
-            width: '18rem'
-          }}
-        >
-          <img
-            alt={product.name}
-            src={product.image}
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              {product.name}
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              {product.price}
-            </CardSubtitle>
-            <CardText>
-              {product.description}
-            </CardText>
-            <Button>
-              Detail
-            </Button>
-          </CardBody>
-        </Card>
-      ))}
-
-
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+          {productGroups.map((group, index) => (
+            <div key={index} style={{ flex: '1 1 25%', maxWidth: '25%' }}>
+              {group.map(product => (
+                <Card
+                  key={product.id}
+                  style={{
+                    width: '100%'
+                  }}
+                >
+                  <img
+                    alt={product.name}
+                    src={product.image}
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">
+                      {product.name}
+                    </CardTitle>
+                    <CardSubtitle
+                      className="mb-2 text-muted"
+                      tag="h6"
+                    >
+                      {product.price}
+                    </CardSubtitle>
+                    <CardText>
+                      {product.description}
+                    </CardText>
+                    <Button>
+                      Detail
+                    </Button>
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-
-
-
   );
 }
 
