3f723166da42a87809357e6dbb8fbf0ce30f7eba
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-02 19:07:06 +0300

    A file and folder architecture has been established.

--- core/src/App.js
+++ core/src/App.js
@@ -1,42 +1,34 @@
 import './App.css';
-import React from 'react';
+import React from 'react'
+import Header from './Partials/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Products from './components/Products';
-import { NavLink, Nav, NavItem } from 'reactstrap';
+import Login from './Components/Auth/Login'
+import Register from './Components/Auth/Register';
+import ListProducts from './Containers/Product/ListProducts'
+import Footer from './Partials/Footer';
 
 function App() {
   return (
-    <Router>
-      <Nav
-      >
-        <NavItem>
-          <NavLink
-            active
-            href="/"
-          >
-            Home
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/products">
-            Products
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            disabled
-            href="#"
-          >
-            Login
-          </NavLink>
-        </NavItem>
-      </Nav>
+    <div>
+      <Header /> {Header}
+      
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}>
+          </Route>
+        </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />}>
+          </Route>
+        </Routes>
+        <Routes>
+          <Route path="/products" element={<ListProducts />}>
+          </Route>
+        </Routes>
+      </Router>
+      <Footer /> {Footer}
 
-
-      <Routes>
-        <Route path="/products" element={<Products />}></Route>
-      </Routes>
-    </Router>
+    </div>
   );
 }
 
