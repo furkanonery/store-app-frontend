6bb94db6eae3003e06bad2d51ea4fcba08d819eb
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-02 19:08:11 +0300

    Footer partial created.

---
+++ core/src/Partials/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <div class="footer py-2 mt-3 bg-dark text-white text-center">
+                    <div class="container">
+                        <span class="muted"> Store App </span>
+                    </div>
+                </div>
+                <footer class="py-2 bg-dark text-white text-center">
+                    Copyright © 2023 Furkan Kadir ÖNERYILDIZ
+                </footer>
+            </footer>
+        </div >
+
+
+
+    );
+}
+
+export default Footer;
