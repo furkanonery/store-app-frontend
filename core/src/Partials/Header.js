e03fc72bded8407d47fde37054c8cbecfb16ef51
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-03 07:04:50 +0300

    Categories have been added to the navbar.

--- core/src/Partials/Header.js
+++ core/src/Partials/Header.js
@@ -3,8 +3,31 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
+import { useState, useEffect } from 'react';
+import api from '../Api/api';
+import Button from 'react-bootstrap/esm/Button';
 
 function Header() {
+
+  const [categories, setCategories] = useState([]);
+  const [token, setToken] = useState(localStorage.getItem('authToken'));
+  const user = localStorage.getItem('user')
+
+  function removeToken() {
+    localStorage.removeItem('authToken');
+    setToken(null);
+  }
+
+  useEffect(() => {
+    api.get('/api/categories/')
+      .then(response => {
+        setCategories(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -15,16 +38,27 @@ function Header() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/products">Products</Nav.Link>
             <NavDropdown title="Categories" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Category 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Category 2</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Category 3</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Category 4</NavDropdown.Item>
+              {
+                categories.map(category => (
+
+                  <NavDropdown.Item href={"/products"}>{category.name.toUpperCase()}</NavDropdown.Item>
+                ))
+              }
             </NavDropdown>
           </Nav>
           <Nav className="ml-auto">
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
-            <Nav.Link href="#logout">Logout</Nav.Link>
+            {
+              token ?
+                (
+                  <Button onClick={removeToken}>Logout({user})</Button>
+
+                ) : (
+                  <><Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/register">Register</Nav.Link></>
+                )
+            }
+
+
           </Nav>
         </Navbar.Collapse>
       </Container>
