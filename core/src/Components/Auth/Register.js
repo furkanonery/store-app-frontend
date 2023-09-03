86098e594bfe48b0191f91913974b28ec5fd3e7b
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-04 02:15:26 +0300

    The register page has been connected to the endpoint.

--- core/src/Components/Auth/Register.js
+++ core/src/Components/Auth/Register.js
@@ -1,25 +1,91 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/esm/Container';
+import api from '../../Api/api';
 
 function Register() {
+
+    const [first_name, setFirstname] = useState('');
+    const [last_name, setLastname] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [email, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('/register/', {
+                first_name: first_name,
+                last_name: last_name,
+                username: username,
+                password: password,
+                password2: password2,
+                email: email
+            });
+
+            setErrorMessage('Register successful.');
+
+        } catch (error) {
+            setErrorMessage('Failed to register.');
+        }
+    };
+
     return (
         <Container>
-            <Form>
+            {errorMessage && <p style={
+                errorMessage[0].toString() === 'R' ?
+                    { color: 'green' } :
+                    { color: 'red' }
+            }>{errorMessage}</p>}
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text"
+                        placeholder="Enter First Name"
+                        value={first_name}
+                        onChange={(e) => setFirstname(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text"
+                        placeholder="Enter Last Name"
+                        value={last_name}
+                        onChange={(e) => setLastname(e.target.value)} />
+                </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" />
+                    <Form.Control type="text"
+                        placeholder="Enter username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Re Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password"
+                        placeholder="Re Enter password"
+                        value={password2}
+                        onChange={(e) => setPassword2(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>E-Mail</Form.Label>
+                    <Form.Control type="text"
+                        placeholder="Enter e-mail"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
